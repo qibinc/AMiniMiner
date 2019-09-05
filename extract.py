62d3b54f410d097fbe9a9f4b70f19c246041c5df
Qibin Chen <chenqibin422@gmail.com>
2019-09-05 23:03:37 +0800

    update

--- extract.py
+++ extract.py
@@ -22,8 +22,8 @@ def lcs(X, Y):
     # declaring the array for storing the dp values
     L = [[None] * (n + 1) for i in range(m + 1)]
 
-    """Following steps build L[m + 1][n + 1] in bottom up fashion 
-	Note: L[i][j] contains length of LCS of X[0..i-1] 
+    """Following steps build L[m + 1][n + 1] in bottom up fashion
+	Note: L[i][j] contains length of LCS of X[0..i-1]
 	and Y[0..j-1]"""
     for i in range(m + 1):
         for j in range(n + 1):
@@ -44,7 +44,7 @@ def lcs(X, Y):
 def get_cited_line(content, title):
     scores = []
     for line in content.split("\n"):
-        if "tang" not in line.lower() or "wu" not in line.lower():
+        if "tang" not in line.lower() or "lou" not in line.lower():
             continue
         scores.append((lcs(line.lower(), title.lower()), line))
     if scores == []:
@@ -60,28 +60,23 @@ if __name__ == "__main__":
 
     file_list = get_file_list(args.dir)
 
-    with open('records.tsv', 'w') as tsvfile:
-        book = xlwt.Workbook()
-        sheet = book.add_sheet('sheet1')
-        for row, fname in enumerate(file_list):
-            with open(os.path.join(args.dir, fname)) as f:
-                content = f.read()
-                score, line = get_cited_line(content, args.title)
-                line = line or "0"
-                match = re.findall(r'\[(\d*)\]', line)
-                match = match or ['0']
-                # res = content.index(str(match[0]))
-                # pattern = r'\[^\]*{match[0]}^\]*\]'
-                # print(r'\[^\]*{match[0]}^\]*\]')
-                # res = re.findall(pattern, content)
-                res = re.findall(r'\[[^\]]*' + str(match[0]) + r'[^\]]*\]', content)
-                # res = content.index(match[0])
-                # print(res)
-                if res == []:
-                    res = [content[0]]
+    book = xlwt.Workbook()
+    sheet = book.add_sheet('sheet1')
+    for row, fname in enumerate(file_list):
+        with open(os.path.join(args.dir, fname)) as f:
+            content = f.read()
+            score, line = get_cited_line(content, args.title)
+            line = line or "-1"
+            match = re.findall(r'\[(\d+)\]', line) + re.findall(r'(\d+)\.', line)
+            match = match or ['-1']
+            res = re.findall(r'\[[^\]]*' + str(match[0]) + r'[^\]]*\]', content)
+            if res == []:
+                res = re.findall(re.compile("tang.{1,5}et.{1,3}al", re.IGNORECASE), content)
+            if res == []:
+                res = ""
+            else:
                 res = content.index(res[0])
-                res = content[res-100:res+100].replace('\n', ' ')
-                # res = '.'.join(res.split('.')[1:-1]) + '.'
-                for col, s in enumerate([fname, score, line, str(match[0]), res]):
-                    sheet.write(row,col,s)
-        book.save('papers.xls')
+                res = content[res-400:res+400].replace('\n', ' ')
+            for col, s in enumerate([fname, line, str(match[0]), res]):
+                sheet.write(row,col,s)
+    book.save('papers.xls')
